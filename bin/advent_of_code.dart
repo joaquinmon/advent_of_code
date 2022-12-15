aff278c7634c759c28f3af438fae03d6582ccf3f
Joaquin M <joaquin.monterocarbonero@colegio-losnaranjos.com>
2022-12-15 23:17:35 +0100

    2022 Dia 01

---
+++ bin/advent_of_code.dart
@@ -0,0 +1,14 @@
+import 'package:advent_of_code/2022/source/dia01.dart' as dia01;
+import 'package:advent_of_code/advent_of_code.dart' as advent_of_code;
+
+
+
+
+void main(List<String> arguments) {
+  
+  print('Dia 01: ');  dia01.main();
+   
+   
+   
+  
+}
---
+++ test/advent_of_code_test.dart
@@ -0,0 +1,6 @@
+import 'package:advent_of_code/advent_of_code.dart';
+import 'package:test/test.dart';
+
+void main() {
+  
+}
